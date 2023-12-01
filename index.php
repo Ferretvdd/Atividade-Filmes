fb0d9ee0a5dc347642e1cc33e793426dccd7a8ac
Ferretvdd <69170190+Ferretvdd@users.noreply.github.com>
2023-12-01 18:41:40 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Filmes</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<a href="cadastroAtores.php">Cadastro do Ator</a> <br>
+	<a href="cadastroDiretores.php">Cadastro do Diretor</a> <br>
+	<a href="cadastroGeneros.php">Cadastro do Genero</a> <br>
+	<a href="cadastro.php">Cadastro dos Filmes</a> <br>
+	<a href="cadAtor.php">Cadastro dos Atores no filme</a> <br>
+	<a href="ConsultarFilme.php">Listagem Filmes</a> <br>
+</body>
+</html>
