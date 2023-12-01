bfe6d246fe8bd2440bb0b2662a7abd33757edb5d
Ferretvdd <69170190+Ferretvdd@users.noreply.github.com>
2023-12-01 18:39:22 -0300

    Create cadastroAtores.php

---
+++ cadastroAtores.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cadastro de Atores</title>
+	<meta charset="utf-8">
+</head>
+	<body>		
+		<form method="POST" action="processa_cad_Ator.php">
+			Ator: <input type="text" name="txtAtor"><br><br><br><br>
+
+			<input type="submit" value="Cadastrar">		
+		</form>
+	</body>
+</html>
